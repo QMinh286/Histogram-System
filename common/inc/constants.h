81db3eef3ad30f103a7743e7d47c8d6fcc3b0325
QMinh286 <minhvq2806@gmail.com>
2025-04-11 03:32:09 -0400

    add constant values

--- common/inc/constants.h
+++ common/inc/constants.h
@@ -13,8 +13,14 @@
 
 #define DP1_SLEEP_TIME 2000000 
 #define DP2_SLEEP_TIME 50000  
+#define DC_READ_SLEEP_TIME 2000000
+#define DC_DISPLAY_INTERVAL 5 
 
 #define DP1_PROCESS "./bin/dp1"
 #define DP2_PROCESS "../DP-2/bin/dp2"
 #define DC_PROCESS "../DC/bin/dc"
+
+#define HISTOGRAM_ONES '-'
+#define HISTOGRAM_TENS '+'
+#define HISTOGRAM_HUNDREDS '*
 #endif /* CONSTANTS_H */
